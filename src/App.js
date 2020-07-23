7a58205fc2897709074a359dc19d56b8f8914424
saiashish90 <saiashish90@gmail.com>
2020-07-23 11:52:13 +0530

    Login moved to index and song check every 5s

--- src/App.js
+++ src/App.js
@@ -1,66 +1,33 @@
-import React, { useState } from 'react';
-import { Client, TrackHandler, PlaylistHandler } from 'spotify-sdk';
+import React from 'react';
 
 var Spotify = require('spotify-web-api-js');
 
-let client = Client.instance;
-
-client.settings = {
-	clientId     : 'fd7a287cc4fa479490cd3ee3ccbfb96d',
-	secretId     : 'fd7a287cc4fa479490cd3ee3ccbfb96d',
-	scopes       : [
-		'user-read-currently-playing',
-		'user-read-playback-state',
-		'playlist-read-private',
-		'playlist-read-collaborative',
-		'user-read-currently-playing'
-	],
-	redirect_uri : 'http://localhost:3000'
-};
-
-/*
- * Login user
- * This is a way, you can do it however you want
- */
-function session() {
-	if (sessionStorage.token) {
-		client.token = sessionStorage.token;
-	} else if (window.location.hash.split('&')[0].split('=')[1]) {
-		sessionStorage.token = window.location.hash.split('&')[0].split('=')[1];
-		client.token = sessionStorage.token;
-	}
-}
-session();
-function login() {
-	client.login().then((url) => {
-		window.location.href = url;
-	});
-}
-
-var spotifyAPI = new Spotify();
-spotifyAPI.setAccessToken(client.token);
-
-function App() {
-	const [ playing, setPlaying ] = useState('Fetching');
-
-	spotifyAPI.getMyCurrentPlayingTrack().then(
-		function(data) {
-			console.log(data);
-			if (data) {
-				setPlaying(data.item.name);
-			} else {
-				setPlaying('No song');
+function App(props) {
+	var spotifyAPI = new Spotify();
+	spotifyAPI.setAccessToken(props.token);
+	function getSong() {
+		spotifyAPI.getMyCurrentPlayingTrack().then(
+			function(data) {
+				console.log(data);
+				if (data) {
+					document.getElementById('playing').innerHTML = data.item.name;
+				} else {
+					document.getElementById('playing').innerHTML = 'No song';
+				}
+			},
+			function(err) {
+				console.error(err);
 			}
-		},
-		function(err) {
-			console.error(err);
-		}
-	);
+		);
+
+		setTimeout(getSong, 5000);
+	}
+
+	getSong();
 
 	return (
 		<div>
-			<h1 id="playing">{playing} </h1>
-			<button onClick={login}>Login</button>
+			<h1 id="playing">Fetching</h1>
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Client } from 'spotify-sdk';
+
 import './index.css';
 import App from './App';
+
+let client = Client.instance;
+
+client.settings = {
+	clientId     : 'fd7a287cc4fa479490cd3ee3ccbfb96d',
+	secretId     : 'fd7a287cc4fa479490cd3ee3ccbfb96d',
+	scopes       : [
+		'user-read-currently-playing',
+		'user-read-playback-state',
+		'playlist-read-private',
+		'playlist-read-collaborative',
+		'user-read-currently-playing'
+	],
+	redirect_uri : 'http://localhost:3000'
+};
+let button = (
+	<button id="login" onClick={login}>
+		Login
+	</button>
+);
+
+function session() {
+	if (sessionStorage.token) {
+		client.token = sessionStorage.token;
+		console.log(client.token);
+		button = <p>Logged in</p>;
+	} else if (window.location.hash.split('&')[0].split('=')[1]) {
+		sessionStorage.token = window.location.hash.split('&')[0].split('=')[1];
+		client.token = sessionStorage.token;
+		button = <p>Logged in</p>;
+		console.log(client.token);
+	}
+}
+session();
+function login() {
+	client.login().then((url) => {
+		window.location.href = url;
+	});
+}
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<App token={client.token} />
+		{button}
 	</React.StrictMode>,
 	document.getElementById('root')
 );
